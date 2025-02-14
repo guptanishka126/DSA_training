4399df8b5e2b4ab8ac3457dd930370972f31c190
guptanishka126 <130595393+guptanishka126@users.noreply.github.com>
2025-02-14 14:54:20 +0530

    231. Power of Two.cpp

---
+++ 231. Power of Two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return 0;
+        } else if(n==1){
+            return 1;
+        } else {
+            return (n%2==0) && isPowerOfTwo(n/2);
+        }
+    }
+};
