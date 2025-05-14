773a94e3ef02031af0882e815f4734677cc25302
guptanishka126 <130595393+guptanishka126@users.noreply.github.com>
2025-05-14 20:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (95.32%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
         double kel, fah;
-        kel = celsius + 273.15;
-        fah = celsius * 1.80 + 32.00;
-
+        kel = celsius +273.15;
+        fah = celsius *1.80 + 32.00;
         return {kel,fah};
     }
+    
 };
